5218ec709a603a0f60b8b9d982b4fb7503208cfd
bakkeshks <bakkeshpruthvi49@gmail.com>
2024-04-16 18:21:33 +0530

    Added feature: render logout only if token exists

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -7,6 +7,9 @@ import Logout from "./Logout";
 function Nav() {
   const { title } = useContext(DsaContext); // Destructure the context value
 
+  // Check if token exists in local storage
+  const tokenExists = localStorage.getItem("token");
+
   return (
     <>
       <nav className="bg-gray-800 p-4">
@@ -18,54 +21,57 @@ function Nav() {
           </div>
           <div className="hidden md:block">
             <ul className="flex space-x-4">
-              <>
-                <li>
-                  <Link
-                    to="/"
-                    className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/addquestion"
-                    className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
-                  >
-                    Add Question
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/viewquestion"
-                    className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
-                  >
-                    View Question
-                  </Link>
-                </li>
+              <li>
+                <Link
+                  to="/"
+                  className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/addquestion"
+                  className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+                >
+                  Add Question
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/viewquestion"
+                  className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+                >
+                  View Question
+                </Link>
+              </li>
+              {/* Render Logout component only if token exists */}
+              {tokenExists && (
                 <li>
                   <Logout />
                 </li>
-              </>
-
-              <>
-                <li>
-                  <Link
-                    to="/signin"
-                    className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
-                  >
-                    SignIn
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/signup"
-                    className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
-                  >
-                    SignUp
-                  </Link>
-                </li>
-              </>
+              )}
+              {/* Render SignIn and SignUp links only if token doesn't exist */}
+              {!tokenExists && (
+                <>
+                  <li>
+                    <Link
+                      to="/signin"
+                      className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+                    >
+                      SignIn
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/signup"
+                      className="text-white hover:text-gray-300 transition duration-300 ease-in-out"
+                    >
+                      SignUp
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
